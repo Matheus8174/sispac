0ff6340c324604dcac3723ab4a500ca862ed286c
Matheus Araújo <souzamatheus8174@gmail.com>
2024-11-03 14:53:30 -0300

    refactor: change root Layout add tab router

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,24 +1,39 @@
 import '../../global.css';
 
 import React from 'react';
+import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
 import { useThemeConfig } from '@/hooks/use-theme-config';
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { ThemeProvider } from '@react-navigation/native';
+
+export const unstable_settings = {
+  initialRouteName: '(app)'
+};
 
 function RootLayout() {
   const theme = useThemeConfig();
 
   return (
-    <ThemeProvider value={theme}>
-      <Stack
-        screenOptions={{
-          navigationBarColor: theme.colors.background,
-          statusBarStyle: theme.dark ? 'light' : 'dark',
-          statusBarColor: theme.colors.background,
-          headerShown: false
-        }}
-      />
-    </ThemeProvider>
+    <GestureHandlerRootView
+      style={{ flex: 1, backgroundColor: theme.colors.background }}
+    >
+      <ThemeProvider value={theme}>
+        <BottomSheetModalProvider>
+          <Stack
+            screenOptions={{
+              navigationBarColor: theme.colors.background,
+              statusBarStyle: theme.dark ? 'light' : 'dark',
+              statusBarColor: theme.colors.background,
+              headerShown: false
+            }}
+          >
+            <Stack.Screen name="(app)" />
+          </Stack>
+        </BottomSheetModalProvider>
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
 
